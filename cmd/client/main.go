9ad1cd8745d298188d646af229f60d389580b0e8
Oleg Kozyrev <okozyrev@ozon.ru>
2022-09-18 13:20:29 +0300

    fix out stats

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -51,7 +52,7 @@ func main() {
 
 	for {
 		data, errRecv := stream.Recv()
-		if errRecv == io.EOF {
+		if errors.Is(err, io.EOF) {
 			fmt.Println("statistics collection completed")
 			break
 		}
@@ -59,6 +60,25 @@ func main() {
 			log.Fatalf(" failed to get stats, err = %v", err)
 		}
 
-		log.Println(data)
+		printStats(data)
 	}
+}
+
+func printStats(data *desc.StartMonitoringResponse) {
+	fmt.Println("\nCPU:")
+	fmt.Println("\tuser mode time:", data.GetCpuInfo().GetUserModeTime())
+	fmt.Println("\tsystem mode time:", data.GetCpuInfo().GetSystemModeTime())
+	fmt.Println("\tidle time:", data.GetCpuInfo().GetIdleTime())
+
+	fmt.Println("Disk:")
+	fmt.Println("\tkbt:", data.GetDiskInfo().GetKbt())
+	fmt.Println("\ttps:", data.GetDiskInfo().GetTps())
+	fmt.Println("\tmbs:", data.GetDiskInfo().GetMbs())
+
+	fmt.Println("Avg load:")
+	fmt.Println("\tavg load 1min:", data.GetLoadInfo().GetLoad1Min())
+	fmt.Println("\tavg load 5min:", data.GetLoadInfo().GetLoad5Min())
+	fmt.Println("\tavg load 15min:", data.GetLoadInfo().GetLoad15Min())
+
+	fmt.Println("collected at:", data.GetCollectedAt().AsTime())
 }
