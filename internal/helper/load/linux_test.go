6c962c5102c367f153e4b9467eca97f0abd4b873
Oleg Kozyrev <okozyrev@ozon.ru>
2022-09-21 22:54:42 +0300

    add tests

---
+++ internal/helper/load/linux_test.go
@@ -0,0 +1,24 @@
+//go:build linux
+// +build linux
+
+package load
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetStat(t *testing.T) {
+	t.Run("test success get stats", func(t *testing.T) {
+		loadInfo, err := GetStats()
+
+		require.NoError(t, err)
+		require.NotNil(t, loadInfo.Load1Min)
+		require.IsType(t, 1.0, loadInfo.Load1Min)
+		require.NotNil(t, loadInfo.Load5Min)
+		require.IsType(t, 1.0, loadInfo.Load5Min)
+		require.NotNil(t, loadInfo.Load15Min)
+		require.IsType(t, 1.0, loadInfo.Load15Min)
+	})
+}
